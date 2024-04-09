0f8c1faa02a9921a4cf34275a6f192d8d7588236
Piyush Priyadarshan Jha <153946249+piyush11p@users.noreply.github.com>
2024-04-09 21:37:08 +0000

    welcome

---
+++ hello.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class hello{
+    public static void main(String args[]){
+      /*   int a =5;
+        int b=4;
+        System.out.println(a*b);
+        int sum=a+b;
+        System.out.println(sum);
+        boolean var = false;
+        System.out.println(var);
+        byte p=50;
+        System.out.println(p);*/
+        //hello i want to become a bestttt
+        Scanner sc=new Scanner(System.in);
+       /*  int a = sc.nextInt();
+        int b= sc.nextInt();
+        int sum =a+b;
+        int product=a*b;
+        System.out.println(sum);
+        System.out.println(product);*/
+        //finding area of circle by taking input from user
+        double radius=sc.nextDouble();
+        double pi=3.14;
+        double area =pi*radius*radius;
+        System.out.println(area);
+
+
+    }
+}
