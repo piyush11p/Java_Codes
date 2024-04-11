23aee6d4fc3f381c022a963b81e41388b407a98f
Piyush Priyadarshan Jha <153946249+piyush11p@users.noreply.github.com>
2024-04-11 20:42:23 +0000

    2nd day

---
+++ code.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class code{
+    public static void main(String args[]){
+       // float a =5.0f;
+       // int b=(int)a;
+       // System.out.println(b);
+       // int $ = 5;
+      //  System.out.println($);
+        byte b = 4;
+        char c ='a';
+        short s = 512;
+        int i = 1000;
+        float f = 3.14f;
+        double d=99.54;
+       double result=(f*b)+(i%c)-(d*s);
+        System.out.println(result);
+
+
+    }
+}
